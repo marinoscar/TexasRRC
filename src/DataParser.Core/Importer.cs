4cfe873f5f862c35213a1c316a3aa64660257f1c
Oscar Marin <oscar@marin.cr>
2025-02-21 13:50:13 -0600

    4

--- src/DataParser.Core/Importer.cs
+++ src/DataParser.Core/Importer.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,16 +14,18 @@ namespace DataParser.Core
         private readonly string _connectionString;
         private readonly ILogger _logger;
         private readonly string _sourceFilePath;
+        private readonly ImporterFactory _importerFactory;
 
         public Importer(string connectionString, string sourceFilePath, ILogger logger)
         {
             _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
             _sourceFilePath = sourceFilePath ?? throw new ArgumentNullException(nameof(sourceFilePath));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _importerFactory = new ImporterFactory();
         }
 
 
-        private void Execute()
+        public void Execute()
         {
             var file = new FileInfo(_sourceFilePath);
             if (!file.Exists)
@@ -29,13 +33,34 @@ namespace DataParser.Core
                 _logger.LogError($"File not found: {_sourceFilePath}");
                 return;
             }
+            var action = _importerFactory.Get(file.Name);
             var reader = new FlatFileReader(_logger);
-            reader.WhileReadingLine(file, line =>
+            var sb = new StringBuilder();
+            var counter = 0;
+            using (var conn = new SqlConnection(_connectionString))
             {
-                var district = GpDistrict.CreateFromText(string.Join("}", parts));
-                var sql = district.ToSqlInsert();
-                ExecuteSql(sql);
-            });
-
+                conn.Open();
+                reader.WhileReadingLine(file, line =>
+                {
+                    sb.AppendLine(action(line));
+                    counter++;
+                    if (counter > 100)
+                    {
+                        RunSql(sb.ToString(), conn);
+                        counter = 0;
+                        sb.Clear();
+                    }
+                });
+            }
         }
+
+        private void RunSql(string command, IDbConnection connection)
+        {
+            using (var cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = command;
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
 }
