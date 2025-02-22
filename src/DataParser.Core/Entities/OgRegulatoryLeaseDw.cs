a14811d3635cd08e357ea76ec7e32603a978464a
Oscar Marin <oscar@marin.cr>
2025-02-21 19:58:48 -0600

    1

--- src/DataParser.Core/Entities/OgRegulatoryLeaseDw.cs
+++ src/DataParser.Core/Entities/OgRegulatoryLeaseDw.cs
@@ -51,7 +51,7 @@ namespace DataParser.Core.Entities
 
             for (int i = 0; i < parts.Length; i++)
             {
-                parts[i] = parts[i].Trim();
+                parts[i] = parts[i].Trim().ToSql();
             }
 
             return new OgRegulatoryLeaseDw
